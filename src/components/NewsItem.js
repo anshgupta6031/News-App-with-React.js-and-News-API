08e8bfff994bc20652709a58d05a2491464b5ca8
Ansh Gupta <guptaansh6031@gmail.com>
2024-07-07 16:07:10 +0530

    video 23 completed

---
+++ src/components/NewsItem.js
@@ -0,0 +1,18 @@
+
+
+
+
+import React, { Component } from 'react'
+
+export default class NewsItem extends Component {
+  render() {
+    return (
+      <div>
+        nitem
+      </div>
+    )
+  }
+}
+
+
+
